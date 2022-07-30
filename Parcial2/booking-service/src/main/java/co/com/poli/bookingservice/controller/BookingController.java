4a31ab46d3fc2558cdd2cd817939c58a817eae91
Jose Restrepo <jos397@hotmail.com>
2022-07-30 14:09:54 -0500

    Micro booking

--- Parcial2/booking-service/src/main/java/co/com/poli/bookingservice/controller/BookingController.java
+++ Parcial2/booking-service/src/main/java/co/com/poli/bookingservice/controller/BookingController.java
@@ -18,7 +18,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/showtimes")
+@RequestMapping("/bookings")
 @RequiredArgsConstructor
 public class BookingController {
 
